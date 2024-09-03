6b284a539769f862e5e329d364c02bea9f8d26e5
Andy Thurman <andythurman@gmail.com>
2024-09-03 09:48:45 +0100

    Replace defaultProps with default params in SwipeRating.tsx

--- src/SwipeRating.tsx
+++ src/SwipeRating.tsx
@@ -196,20 +196,22 @@ export default class SwipeRating extends Component<
   SwipeRatingProps,
   SwipeRatingState
 > {
-  static defaultProps = {
-    type: "star",
-    ratingImage: STAR_IMAGE,
-    ratingColor: "#f1c40f",
-    ratingBackgroundColor: "white",
-    ratingCount: 5,
-    showReadOnlyText: true,
-    imageSize: 40,
-    minValue: 0,
-    jumpValue: 0,
-  };
   ratingRef: any;
 
-  constructor(props) {
+  constructor(overrideProps) {
+    const props = {
+      type: "star",
+      ratingImage: STAR_IMAGE,
+      ratingColor: "#f1c40f",
+      ratingBackgroundColor: "white",
+      ratingCount: 5,
+      showReadOnlyText: true,
+      imageSize: 40,
+      minValue: 0,
+      jumpValue: 0,
+      ...overrideProps
+    }
+
     super(props);
     const { onStartRating, onSwipeRating, onFinishRating, fractions } =
       this.props;
